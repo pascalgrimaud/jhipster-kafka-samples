c768b58445fbcf94dd35f42d2237e08dad3fe8e8
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-04-26 23:03:10 +0200

    Add compose and k8s configuration

---
+++ compose/README-DOCKER-COMPOSE.md
@@ -0,0 +1,19 @@
+# JHipster generated Docker-Compose configuration
+
+## Usage
+
+Launch all your infrastructure by running: `docker-compose up -d`.
+
+## Configured Docker services
+
+### Applications and dependencies:
+
+- HipsReader (monolith application)
+- HipsReader's postgresql database
+- HipsWriter (monolith application)
+- HipsWriter's mysql database
+
+### Additional Services:
+
+- Kafka
+- Zookeeper
---
+++ k8s/kubectl-apply.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+logSummary(){
+    echo ""
+    echo "#####################################################"
+    echo "Please find the below useful endpoints,"
+    echo "#####################################################"
+}
+
+kubectl apply -f hipsreader/
+kubectl apply -f hipswriter/
+kubectl apply -f messagebroker/
+
+logSummary
